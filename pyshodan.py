7363c67088becb39373951f6490eb7dd94af0a08
Cristian Gutierrez <cdgutierrez456@gmail.com>
2026-04-15 16:49:24 -0500

    Implementacion multithreeds

--- pyshodan.py
+++ pyshodan.py
@@ -1,8 +1,21 @@
+import os
+import time
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from dotenv import load_dotenv
 from shodansearch import ShodanSearch
 from login_automatization import has_valid_credentials
 
-import os
+def get_results(results, index):
+    res = {
+        'index': index,
+        'data': f'\nResultado {index}\n'
+                f"Direccion IP: {results['ip_str'] if results else 'no_ip'}\n"
+                f"Hostname: {results['hostnames'] if results else 'no_hostname'}\n"
+                f"Localizacion: {results['location'] if results else 'no_location'}\n"
+                f"Credenciales por defecto: {has_valid_credentials(results)}\n"
+    }
+    return res
 
 def main():
     load_dotenv()
@@ -16,12 +29,19 @@ def main():
     if not results:
         return
 
-    for i in range(10):
-        print(f'\nResultado {i}')
-        print(f"Direccion IP: {results['matches'][i]['ip_str'] if results else 'no_ip'}")
-        print(f"Hostname: {results['matches'][i]['hostnames'] if results else 'no_hostname'}")
-        print(f"Localizacion: {results['matches'][i]['location'] if results else 'no_location'}")
-        print(f"Credenciales por defecto: {has_valid_credentials(results['matches'][i])}")
+    t = time.perf_counter()
+
+    workers = []
+
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for i in range(5):
+            workers.append(executor.submit(get_results, results['matches'][i], i))
+
+    for worker in as_completed(workers):
+        result = worker.result()
+        print(result['data'])
+
+    print(f'Tiempo de procesamiento: {time.perf_counter() - t:.2f}s')
 
 if __name__ == "__main__":
     main()
